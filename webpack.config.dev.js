a92e335e36f6f9b9775cc98da745a7d91bd74da1
Jose Vilca Campana @marcusmors <alivezeh@gmail.com>
2021-10-25 23:25:51 -0500

    :wrench: prod & dev bundler conf files
    #

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,3 +1,18 @@
+// Copyright (C) 2021 José Enrique Vilca Campana
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 // webpack = require("webpack")
 // const CopyPlugin = require("copy-webpack-plugin")
 
@@ -9,6 +24,7 @@ const BundleAnalyzerPlugin =
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 const config = {
+	mode: "development",
 	entry: "./src/index.ts",
 	output: {
 		path: path.resolve(__dirname, "dist"),
---
+++ webpack.config.prod.js
@@ -0,0 +1,66 @@
+// Copyright (C) 2021 José Enrique Vilca Campana
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// webpack = require("webpack")
+// const CopyPlugin = require("copy-webpack-plugin")
+
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+const config = {
+	mode: "production",
+	entry: "./src/index.ts",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: "babel-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.s?css|\.sass$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.ts(x)?$/,
+				loader: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: "./public/index.html",
+			filename: "./index.html",
+		}),
+		new MiniCssExtractPlugin(),
+		// new CopyPlugin({
+		// 	patterns: [{ from: "public/index.html" }],
+		// }),
+		new CleanWebpackPlugin(),
+	],
+}
+
+module.exports = config
